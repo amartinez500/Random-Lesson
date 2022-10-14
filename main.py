7b03110a0c5b7b48216a93bb5f2865164f309df6
Arturo Martinez <113930370+amartinez500@users.noreply.github.com>
2022-10-14 17:13:44 +0000

    how to tell if a number even or odd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from random import shuffle
   # random
  # Python comes with a built in random library. There are a lot of functions included in this 
  #random library, so we will only 
@@ -17,3 +18,22 @@ else: print("not doubles")
 if dice1 and dice2 ==1:
   print("you rolled snake eyes")
 else: print("you did not roll snake eyes")
+
+# practice with shuffle
+my_list = range(1,51)
+print("my new list")
+print(list(my_list))
+my_list = list(my_list)
+shuffle(my_list)
+print(my_list)
+
+random = randint(1,201)
+print(random)
+
+if (random % 2) == 0:
+  print("{0} is Even number". format(random))
+else:
+  print("{0} is Odd number". format(random))
+
+
+
