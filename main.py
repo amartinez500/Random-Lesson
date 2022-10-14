59b6ad67ced65cc9d57541b2d1ec16f37a26754e
Arturo Martinez <113930370+amartinez500@users.noreply.github.com>
2022-10-14 16:55:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from random import randint
+  # random
+ # Python comes with a built in random library. There are a lot of functions included in this 
+ #random library, so we will only 
+  #show you two useful functions for now.
+print("random")
+dice1 = randint(1,7)
+print(f"dice1 : {dice1}")
+dice2 = randint(1,7)
+print(f"dice2 : {dice2}")
+rolledDoubles = dice1 == dice2
+if rolledDoubles:
+  print("you rolled doubles")
+else: print("not doubles")
+
+# create a way to roll snakes
+if dice1 and dice2 ==1:
+  print("you rolled snake eyes")
